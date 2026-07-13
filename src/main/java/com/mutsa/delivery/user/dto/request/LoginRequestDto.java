de9b8650d5e348538289639d10e2389ae7e9e98c
leehanjay <128954406+leehanjay@users.noreply.github.com>
2026-07-13 21:25:40 +0900

    [feat]: 로그인 API 구현 (#26)
    * [faet]: 회원가입 API 구현
    
    * [feat]: 로그인 구현
    
    * chore: 로그인 에러코드 유실 방지용 임시 커밋
    
    * [refactor]: 접근제어자 제거
    
    * [chore]:: mport문  제거

---
+++ src/main/java/com/mutsa/delivery/user/dto/request/LoginRequestDto.java
@@ -0,0 +1,21 @@
+package com.mutsa.delivery.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDto {
+
+    @NotNull(message = "이메일은 필수입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
+    @NotNull(message = "비밀번호는 필수입니다.")
+    private String password;
+}

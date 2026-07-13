a561464d700185ee502c0bb94c17981a2888b0dc
leehanjay <128954406+leehanjay@users.noreply.github.com>
2026-07-13 15:48:50 +0900

    [refactor]: AuthErrorCode 추가

--- src/main/java/com/mutsa/delivery/user/service/UserService.java
+++ src/main/java/com/mutsa/delivery/user/service/UserService.java
@@ -2,11 +2,15 @@ package com.mutsa.delivery.user.service;
 
 import com.mutsa.delivery.global.apiPayload.code.GeneralErrorCode;
 import com.mutsa.delivery.global.apiPayload.exception.ProjectException;
+import com.mutsa.delivery.global.security.JwtTokenProvider;
+import com.mutsa.delivery.global.security.exception.AuthErrorCode;
 import com.mutsa.delivery.user.dto.request.SignupRequestDto;
 import com.mutsa.delivery.user.dto.response.UserResponseDto;
 import com.mutsa.delivery.user.entity.User;
 import com.mutsa.delivery.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 

e69f3255f279e73e8ca517de37aa99ceed37b44c
leehanjay <128954406+leehanjay@users.noreply.github.com>
2026-07-14 02:12:37 +0900

    [fix]: 회: SignUpRequestDto 비밀번호 í호 공백 불가 (#32)

--- src/main/java/com/mutsa/delivery/user/dto/request/SignupRequestDto.java
+++ src/main/java/com/mutsa/delivery/user/dto/request/SignupRequestDto.java
@@ -1,6 +1,7 @@
 package com.mutsa.delivery.user.dto.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,7 +15,7 @@ public class SignupRequestDto {
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
 
-    @NotNull(message = "비밀번호는 필수입니다.")
+    @NotBlank(message = "비밀번호는 필수입니다.")
     private String password;
 
     @NotNull(message = "닉네임은 필수입니다.")

995d85b51ee5e81e741b446d1ae7d91668d9047a
Seunghwan Song <bourne11594@gmail.com>
2026-07-21 17:32:11 +0900

    [fix]: CustomOAuth2UserService 소셜 가입 시 이메일 unique 충돌 예외 처리

--- src/main/java/com/mutsa/delivery/global/security/CustomOAuth2UserService.java
+++ src/main/java/com/mutsa/delivery/global/security/CustomOAuth2UserService.java
@@ -4,10 +4,12 @@ import com.mutsa.delivery.user.entity.SocialType;
 import com.mutsa.delivery.user.entity.User;
 import com.mutsa.delivery.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
@@ -42,9 +44,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         String nickname = profile == null ? null : (String) profile.get("nickname");
 
         User user = userRepository.findBySocialTypeAndSocialId(SocialType.KAKAO, socialId)
-                .orElseGet(() -> userRepository.save(
-                        User.createSocial(email, resolveNickname(nickname), SocialType.KAKAO, socialId)
-                ));
+                .orElseGet(() -> createUser(email, nickname, socialId));
 
         Map<String, Object> customAttributes = new HashMap<>(attributes);
         customAttributes.put("userId", user.getUserId());
@@ -55,6 +55,17 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                 customAttributes,
                 "id"
         );
+    }
+
+    // 카카오 이메일이 이미 일반 회원가입에 쓰인 경우 등 unique 제약 위반은
+    // OAuth2AuthenticationException으로 변환해 OAuth2FailureHandler가 리다이렉트로 처리하게 함
+    private User createUser(String email, String nickname, String socialId) {
+        try {
+            return userRepository.save(User.createSocial(email, resolveNickname(nickname), SocialType.KAKAO, socialId));
+        } catch (DataIntegrityViolationException e) {
+            throw new OAuth2AuthenticationException(
+                    new OAuth2Error("social_login_failed", "이미 가입된 이메일이거나 처리 중 오류가 발생했습니다.", null), e);
+        }
     }
 
     // 카카오 기본 닉네임("카카오유저")이 겹치는 경우를 대비해 닉네임 unique 제약 위반 없이 가입되도록 처리
